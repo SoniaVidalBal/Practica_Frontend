d3e654e4bd80de5e496a840f019589d6533b32a3
Sonia Vidal <soniaavidaal@gmail.com>
2023-01-19 20:17:02 +0100

    Intento mostrar objectos

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,26 +2,26 @@
   <div class="about">
     <h3>This is a products page</h3>
   </div>
+  <div v-for="product in products"
+  :key="product">
+  <h4>
+  {{ product }}
+  </h4>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { AxiosResponse } from 'axios';
 import soniapopApi from '../api/soniapopApi';
 import { Products } from '../models/product'
 
   export default defineComponent({
-    props: {
-      id: {
-        type: Number,
-        required: true,
-      }
-    },
     setup(props) {
-      soniapopApi.get<unknown, AxiosResponse<Products[]>>('/products/').then((resp) => {
-        console.log(resp.data)
-      }); 
-      return {};
+      let products = ref<Products>();
+      soniapopApi.get<unknown, AxiosResponse<Products>>('/products')
+        .then((resp) => (products.value = resp.data)); 
+      return {products};
     }
   })
 </script>
