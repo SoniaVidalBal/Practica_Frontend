c8727bf67bb5316c711a883b270159852af32ab0
Sonia Vidal <soniaavidaal@gmail.com>
2023-01-24 19:33:36 +0100

    Autorizacion token

--- src/api/soniapopApi.ts
+++ src/api/soniapopApi.ts
@@ -1,11 +1,15 @@
-import axios from "axios";
+import axios, {AxiosHeaders} from "axios";
 
 const soniapopApi= axios.create({
     baseURL:'https://api.escuelajs.co/api/v1/'
 });
 
-//soniapopApi.interceptors.request.use((config)=>{
-//    config.headers?.set('Authorization')
-//})
+soniapopApi.interceptors.request.use((config)=>{
+    const token = localStorage.getItem('token') ?? '';
+    if (token) {
+        (config.headers as AxiosHeaders).set("Authorisation", `Bearer ${token}`) //JWT
+    }
+    return config;
+})
 
 export default soniapopApi;
